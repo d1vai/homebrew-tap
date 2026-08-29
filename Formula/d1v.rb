872cc9cda08c409077638822e25dfa6af42c2265
aboutmydreams <aboutmydreams@163.com>
2026-08-30 01:46:51 +0900

    d1v 0.1.27

--- Formula/d1v.rb
+++ Formula/d1v.rb
@@ -1,26 +1,26 @@
 class D1v < Formula
   desc "Command-line interface for d1v.ai"
   homepage "https://www.d1v.ai"
-  version "0.1.24"
+  version "0.1.27"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.24/d1v-aarch64-apple-darwin.tar.gz"
-      sha256 "ee14f6d0e97f0a7b7662b91b753681a4ca95d8fb192c3728d20e0160f21c567e"
+      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.27/d1v-aarch64-apple-darwin.tar.gz"
+      sha256 "ad6d247a4a363ec04d494f276795491d1d2486cd20641bca2e1b216df4322173"
     else
-      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.24/d1v-x86_64-apple-darwin.tar.gz"
-      sha256 "33d6c3412fb20ae1096390e8dba340f83a0ad60430c0b78eeb5cdf2fc4b1498c"
+      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.27/d1v-x86_64-apple-darwin.tar.gz"
+      sha256 "6a0b242bde46113b0d42ce68500bba69197e365f9ac55685574ebda5ce6296cb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.24/d1v-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "23e03342342507fc6f9c4893569fda8c25e90e8ad7b8e6ab0648de94b49d2711"
+      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.27/d1v-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7b0871b3f929d1631917020e18f2c3eeb05a3c53768d42b9c538ba972255c0ab"
     else
-      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.24/d1v-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2c4b9fd3091a4cab60c8b298295c839aff8a2c9e71ad6c2cb56b338d1c9669b2"
+      url "https://github.com/d1vai/d1v-cli/releases/download/v0.1.27/d1v-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "29b64777ec9f99baba766f5a2e0a302b718198256ed9d90abb2db16f667cdd47"
     end
   end
 
